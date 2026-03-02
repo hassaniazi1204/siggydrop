afc6ee5f95ce0a22481d6dff47f1aa43dc1be9e6
hassaniazi1204 <166181429+hassaniazi1204@users.noreply.github.com>
2026-03-02 21:38:53 +0500

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,6 +1,8 @@
-import { NextResponse } from 'next/server';
-import { createServerClient } from '@supabase/ssr';
-import { cookies } from 'next/headers';
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
